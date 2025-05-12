ff1d141d1ab1affca06765585940936f8467215c
Unholydeath <thebodily@gmail.com>
2025-05-12 04:58:01 -0600

    Update ArmyNameEnum.cs
    Added existing army names. Will need new chaos daemon names.

--- Warhammer10thEdition/ArmyNameEnum.cs
+++ Warhammer10thEdition/ArmyNameEnum.cs
@@ -8,6 +8,40 @@ namespace Warhammer10thEdition
 {
     public enum KeyWordEnum
     {
-        ADEPUTUS_ASTARTES
+        ADEPUTUS_ASTARTES,
+        ADEPTA_SORORITAS,
+        ADEPTUS_CUSTODES,
+        ADEPTUS_MECHANICUS,
+        BLACK_TEMPLARS,
+        LEGIONES_DAEMONICA,
+        DEATHWATCH,
+        GREY_KNIGHTS,
+        AGENTS_OF_THE_IMPERIUM,
+        IMPERIAL_KNIGHTS,
+        LEAGUES_OF_VOTANN,
+        SALAMANDERS,
+        IMPERIAL_FISTS,
+        IRON_HANDS,
+        RAVEN_GUARD,
+        WHITE_SCARS,
+        ULTRAMARINES,
+        SPACE_WOLVES,
+        THOUSAND_SONS,
+        TYRANIDS,
+        ASTRA_MILITARUM,
+        DEATH_GUARD,
+        LEGIONS_OF_EXCESS,
+        EMPERORS_CHILDREN,
+        WORLD_EATERS,
+        YNNARI,
+        HARLEQUINS,
+        ASURYANI,
+        DRUKHARI,
+        GENESTEALER_CULTS,
+        NECRONS,
+        ORKS,
+        TAU_EMPIRE,
+        HERETIC_ASTARTES,
+        CHAOS_KNIGHTS
     }
 }
